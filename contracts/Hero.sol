caf95e13e007b089419c71801521f36fde849adc
Dima Pasumansky <dpsmnsk@gmail.com>
2023-05-19 23:50:15 +0500

    testing storage methods

--- contracts/Hero.sol
+++ contracts/Hero.sol
@@ -5,7 +5,7 @@ contract Hero {
 
   mapping (address => uint[]) addressToHeroes;
 
-  function generateRandom() public view returns (uint) {
+  function generateRandom() public virtual view returns (uint) {
     return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp)));
   }
 
@@ -14,26 +14,26 @@ contract Hero {
   }
 
   // str is the first 5 bits (after 2 bits for character class)
-  function getStrength(uint hero) public pure returns (uint) {
-    return (hero >> 2) & 0x1F;  
+  function getStrength(uint hero) public pure returns (uint32) {
+    return uint32((hero >> 2) & 0x1F);  
   }
 
   // health is the next 5 bits after str and class
-  function getHealth(uint hero) public pure returns (uint) {
-    return (hero >> 7) & 0x1F;  
+  function getHealth(uint hero) public pure returns (uint32) {
+    return uint32((hero >> 7) & 0x1F);  
   }
 
   // u get the idea..
-  function getDex(uint hero) public pure returns (uint) {
-    return (hero >> 12) & 0x1F;  
+  function getDex(uint hero) public pure returns (uint32) {
+    return uint32((hero >> 12) & 0x1F);  
   }
 
-  function getIntellect(uint hero) public pure returns (uint) {
-    return (hero >> 17) & 0x1F;  
+  function getIntellect(uint hero) public pure returns (uint32) {
+    return uint32((hero >> 17) & 0x1F);  
   }
 
-  function getMagic(uint hero) public pure returns (uint) {
-    return (hero >> 22) & 0x1F;  
+  function getMagic(uint hero) public pure returns (uint32) {
+    return uint32((hero >> 22) & 0x1F);  
   }
 
   function createHero(Class class) public payable {
--- test/Hero.ts
+++ test/Hero.ts
@@ -4,7 +4,7 @@ import { expect } from 'chai';
 
 describe('Hero', function () {
   async function createHero() {
-    const Hero = await ethers.getContractFactory('Hero');
+    const Hero = await ethers.getContractFactory('TestHero');
     const hero = await Hero.deploy();
     await hero.deployed();
 
@@ -20,7 +20,7 @@ describe('Hero', function () {
   it('should get a zero hero array', async function () {
     expect(await hero.getHeroes()).to.deep.equal([]);
   });
-  
+
   it('should fail at creating hero cause of payment', async function () {
     let e;
 
@@ -33,5 +33,15 @@ describe('Hero', function () {
     }
 
     expect(e.message.includes('omg, send more money!!1')).to.equal(true);
+  });
+
+  it('testing creating hero stats', async function () {
+    const hero = await createHero();
+    await hero.setRandom(69);
+    await hero.createHero(0, { value: ethers.utils.parseEther('0.5') });
+    const h = (await hero.getHeroes())[0];
+
+    expect(await hero.getMagic(h)).to.equal(16);
+    expect(await hero.getHealth(h)).to.equal(2);
   });
 });
